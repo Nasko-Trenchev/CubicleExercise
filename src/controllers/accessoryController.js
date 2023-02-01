61aa9d55e001a20e88147157769fa717be3ff0f8
Nasko-Trenchev <naskotrenchev1@abv.bg>
2023-02-01 16:44:25 +0200

    Adding accessories to Mongoose

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -7,10 +7,13 @@ router.get('/create', (req, res) =>{
     res.render('createAccessory');
 })
 
-router.post('/create', (req, res) =>{
+router.post('/create', async (req, res) =>{
 
-    
+    const {name, description, imageUrl} = req.body;
+
+    const accessory = new Accessory({name, description, imageUrl});
+    await accessory.save()
+    res.redirect('/');
 })
-
 
 module.exports = router;
