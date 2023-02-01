5c919ae55f9d01a2d2e10bc79190de4f21b43ad9
Nasko-Trenchev <naskotrenchev1@abv.bg>
2023-02-01 16:20:20 +0200

    Add accessory model

---
+++ src/models/Accessory.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const accessoryShema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+        match: [/^https?:\/\//, "Invalid URL"]
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLenght: 50
+    }
+})
+
+const Accessory = mongoose.model('Accessory', accessoryShema);
+
+module.exports = Accessory;
