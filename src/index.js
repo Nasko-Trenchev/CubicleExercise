7944b79511aec57f0b31fdaaefd75dd90c94f192
Nasko-Trenchev <naskotrenchev1@abv.bg>
2023-02-01 14:32:05 +0200

    Adding resources and HBS setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
+
 const config = require('./config');
+const setupViewEngine = require('./config/setupViewEngine');
+const routs = require("./routes");
+
 
 const app = express();
 
+setupViewEngine(app);
 
-
-
+app.use(express.static('src/public'))
+app.use(express.urlencoded({extended: false}));
+app.use(routs);
 
 
 app.listen(config.PORT, () =>{
