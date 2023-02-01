2726ce5e266c0414cb8851afcfdb686c8fe606ea
Nasko-Trenchev <naskotrenchev1@abv.bg>
2023-02-01 14:55:10 +0200

    Adding database

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const config = require('./config');
+const initDatabase = require("./config/databaseInit");
 const setupViewEngine = require('./config/setupViewEngine');
 const routs = require("./routes");
 
@@ -13,7 +14,9 @@ app.use(express.static('src/public'))
 app.use(express.urlencoded({extended: false}));
 app.use(routs);
 
-
+initDatabase()
+.then(
 app.listen(config.PORT, () => {
     console.log("Server is running on port 5000...")
-})
+}))
+.catch((err) => console.log(err.message));
