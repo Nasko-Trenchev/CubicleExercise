fbd8a724983aee46efaefe69df73df24fc8d1d0c
Nasko-Trenchev <naskotrenchev1@abv.bg>
2023-02-03 12:12:23 +0200

    Adding new resources

---
+++ src/controllers/authController.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+
+
+router.get('/login', (req, res) =>{
+
+
+})
+
+
+module.exports = router;
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,15 @@
 const router = require("express").Router();
 const homeController = require('./controllers/homeController');
 const cubeController = require('./controllers/cubeController');
-const accessoryControler = require('./controllers/accessoryController');
+const accessoryController = require('./controllers/accessoryController');
+const authController = require('./controllers/authController');
 
 
 router.get('/', homeController.getHomePage);
 router.get('/about', homeController.getAboutPage);
 router.get('/404', homeController.getErrorPage);
+
+router.use('/', authController);
 
 router.get(`/cubes/:cubeId/details`, cubeController.getDetailsPage)
 
@@ -15,6 +18,6 @@ router.post('/create', cubeController.postCreatePage);
 router.get('/cubes/:cubeId/attach', cubeController.getDetailsAttachPage);
 router.post('/cubes/:cubeId/attach', cubeController.postDetailsAttachPage);
 
-router.use('/accessories', accessoryControler);
+router.use('/accessories', accessoryController);
 
 module.exports = router;
