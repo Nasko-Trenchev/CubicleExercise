7c0e5e5c2ce40f8f5aa564f80d06d843c90b6a49
Nasko-Trenchev <naskotrenchev1@abv.bg>
2023-02-03 14:59:30 +0200

    Send cookie to client

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -12,10 +12,11 @@ router.post('/login', async (req, res) =>{
     try{
       const token = await authService.login(username, password)
       console.log(token);
+      res.cookie('auth', token, {httpOnly: true});
+
     }
     catch(err){
         console.log(err);
-        res.redirect('/');
     }
    
     res.redirect('/');
